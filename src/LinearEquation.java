52ea91e6e966b57a2d2f3456b19d0516842bd287
DariuszSm <dariuszs2@nycstudents.net>
2023-10-04 14:02:03 -0400

    first draft of running, looping code

---
+++ src/LinearEquation.java
@@ -0,0 +1,56 @@
+public class LinearEquation {
+    private int x1;
+    private int x2;
+    private int y1;
+    private int y2;
+
+    private double slope;
+    private double yIntercept;
+    private double distance;
+    private String equation;
+
+    public void parseCoordinates(String rawCoord1, String rawCoord2) {
+        x1 = Integer.parseInt(rawCoord1.substring(1, rawCoord1.indexOf(",")));
+        y1 = Integer.parseInt(rawCoord1.substring(rawCoord1.indexOf(",") + 1, rawCoord1.length() - 1));
+
+        x2 = Integer.parseInt(rawCoord2.substring(1, rawCoord2.indexOf(",")));
+        y2 = Integer.parseInt(rawCoord2.substring(rawCoord2.indexOf(",") + 1, rawCoord2.length() - 1));
+    }
+
+    private void calcSlope() {
+        slope = (double)(y2-y1)/(x2-x1);
+    }
+
+    private void calcYIntercept() {
+        yIntercept = y1 - (slope * x1);
+    }
+
+    private void setEquation() {
+        equation = "y = " + (y2-y1) + "/" + (x2-x1) + "x + " + yIntercept;
+    }
+
+    private void setDistance() {
+        distance = Math.sqrt(
+                Math.pow((double)x2-x1, 2) + Math.pow((double)y2-y1, 2)
+        );
+    }
+
+    public String solveForCustomX(int customX) {
+        return "(" + (double)customX + "," + ((double)customX * slope + yIntercept) + ")";
+    }
+
+    public LinearEquation(String rawCoord1, String rawCoord2) {
+        parseCoordinates(rawCoord1, rawCoord2);
+        calcSlope();
+        calcYIntercept();
+        setEquation();
+        setDistance();
+    }
+
+    public String toString() {
+        return "Slope of line: " + (double)Math.round(slope * 100) / 100
+                + "\nY-intercept: " + (double)Math.round(yIntercept * 100) / 100
+                + "\nSlope intercept form: " + equation
+                + "\nDistance between points: " + (double)Math.round(distance * 100) / 100;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
+        String firstCoordRaw;
+        String secondCoordRaw;
+        int customX;
+
+        Scanner s = new Scanner(System.in);
+
+        System.out.println("Enter your first Coordinate Point: ");
+        firstCoordRaw = s.nextLine();
+        System.out.println("Enter your second Coordinate Point: ");
+        secondCoordRaw = s.nextLine();
+
+        LinearEquation equation = new LinearEquation(firstCoordRaw, secondCoordRaw);
+        System.out.println(equation);
+
+        while (true) {
+            System.out.println("\nEnter a custom X integer for the equation: ");
+            customX = Integer.parseInt(s.nextLine());
+            System.out.println(equation.solveForCustomX(customX));
+        }
 
     }
 }
